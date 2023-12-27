6e0b72d55c6bc12747d7f95c1b6b4203c74a6fab
DarkMatter-999 <darkmatter999official@gmail.com>
2023-12-27 14:33:12 +0530

    Added 3D Transform

--- src/lve_render_system.cpp
+++ src/lve_render_system.cpp
@@ -19,8 +19,7 @@
 namespace lve {
 
 struct SimplePushConstantData {
-	glm::mat2 transform{1.0f};
-	glm::vec2 offset;
+	glm::mat4 transform{1.0f};
 	alignas(16) glm::vec3 color;
 };
 
@@ -64,20 +63,14 @@ void LveRenderSystem::createPipeline(VkRenderPass renderPass) {
 }
 
 void LveRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<LveGameObject>& gameObjects) {
-	int i = 0;
-	for (auto& obj : gameObjects) {
-		i += 1;
-		obj.transform2d.rotation = glm::mod<float>(obj.transform2d.rotation + 0.001f * i, 2.f * glm::pi<float>());
-	}
-
 	lvePipeline->bind(commandBuffer);
 	for (auto& obj : gameObjects) {
-		obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.01f, glm::two_pi<float>());
+		obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
+		obj.transform.rotation.x = glm::mod(obj.transform.rotation.y + 0.005f, glm::two_pi<float>());
 
 		SimplePushConstantData push{};
-		push.offset = obj.transform2d.translation;
 		push.color = obj.color;
-		push.transform = obj.transform2d.mat2();
+		push.transform = obj.transform.mat4();
 
 		vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
 		
