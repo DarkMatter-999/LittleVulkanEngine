3d20542b2d28ffb06d33301716b325c8061904df
DarkMatter-999 <darkmatter999official@gmail.com>
2023-12-28 10:55:57 +0530

    Added Vertex Indexing and Index buffers

--- src/lve_model.hpp
+++ src/lve_model.hpp
@@ -22,7 +22,12 @@ public:
 		static std::vector<VkVertexInputAttributeDescription> getAttributeDescriptions();
 	};
 
-	LveModel(LveDevice &device, const std::vector<Vertex> &vertices);
+	struct Builder {
+		std::vector<Vertex> vertices{};
+		std::vector<uint32_t> indices{};
+	};
+
+	LveModel(LveDevice &device, const Builder& builder);
 	~LveModel();
 
 	LveModel(const LveModel &) = delete;
@@ -34,11 +39,19 @@ public:
 private:
 
 	void createVertexBuffers(const std::vector<Vertex> &vertices);
+	void createIndexBuffers(const std::vector<uint32_t> &indices);
 
 	LveDevice& lveDevice;
+
 	VkBuffer vertexBuffer;
 	VkDeviceMemory vertexBufferMemory;
 	uint32_t vertexCount;
+
+
+	bool hasIndexBuffer = false;
+	VkBuffer indexBuffer;
+	VkDeviceMemory indexBufferMemory;
+	uint32_t indexCount;
 
 };
 
