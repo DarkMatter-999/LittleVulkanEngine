79245bccd9b58d53b0fb6fb0b216bd81aa21aea9
DarkMatter-999 <darkmatter999official@gmail.com>
2023-12-28 20:20:46 +0530

    Added Diffused lighting

--- shaders/simple.frag
+++ shaders/simple.frag
@@ -5,10 +5,9 @@ layout(location=0) in vec3 inColor;
 layout(location=0) out vec4 outColor;
 
 layout(push_constant) uniform Push {
-	mat4 transform;
-	vec3 color;
+	mat4 transform; //projection * model * view
+	mat4 normalMatrix;
 } push;
-
 
 void main() {
 	outColor = vec4(inColor, 1.0);
--- src/lve_render_system.cpp
+++ src/lve_render_system.cpp
@@ -20,7 +20,7 @@ namespace lve {
 
 struct SimplePushConstantData {
 	glm::mat4 transform{1.0f};
-	alignas(16) glm::vec3 color;
+	glm::mat4 normalMatrix{1.0f};
 };
 
 LveRenderSystem::LveRenderSystem(LveDevice& device, VkRenderPass renderPass) : lveDevice{device} {
@@ -69,8 +69,9 @@ void LveRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vect
 
 	for (auto& obj : gameObjects) {
 		SimplePushConstantData push{};
-		push.color = obj.color;
-		push.transform = projectionView * obj.transform.mat4();
+		auto modelMatrix = obj.transform.mat4();
+		push.transform = projectionView * modelMatrix;
+		push.normalMatrix = obj.transform.normalMatrix();
 
 		vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
 		
