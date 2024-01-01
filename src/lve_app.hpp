DarkMatter-999/LittleVulkanEngine

commit 209fd94584ecb9ced484eacb41c9603922e41635

Added Descriptor sets

src/lve_app.hpp
@@ -5,6 +5,7 @@
 #include "lve_game_object.hpp"
 #include "lve_device.hpp"
 #include "lve_renderer.hpp"
+#include "lve_descriptors.hpp"
 #include "vulkan/vulkan_core.h"
 
 #include <memory>
@@ -33,6 +34,7 @@ private:
 
 	LveRenderer lveRenderer{lveWindow, lveDevice};
 	std::vector<LveGameObject> gameObjects;
+	std::unique_ptr<LveDescriptorPool> globalPool{};
 };
 
 }

shaders/simple.frag
@@ -5,7 +5,7 @@ layout(location=0) in vec3 inColor;
 layout(location=0) out vec4 outColor;
 
 layout(push_constant) uniform Push {
-	mat4 transform; //projection * model * view
+	mat4 modelMatrix;
 	mat4 normalMatrix;
 } push;
 

shaders/simple.vert
@@ -8,18 +8,22 @@ layout(location=3) in vec2 uv;
 layout(location=0) out vec3 fragColor;
 
 layout(push_constant) uniform Push {
-	mat4 transform; //projection * model * view
+	mat4 modelMatrix;
 	mat4 normalMatrix;
 } push;
 
-const vec3 DIRECTION_TO_LIGHT = normalize(vec3(1.0, -3.0, -1.0));
+layout(set=0, binding=0) uniform GlobalUbo {
+	mat4 projectionViewMatrix;
+	vec3 directionToLight;
+} ubo;
+
 const float AMBIENT = 0.02;
 
 void main() {
-	gl_Position = push.transform * vec4(position, 1.0);
+	gl_Position = ubo.projectionViewMatrix * push.modelMatrix * vec4(position, 1.0);
 
 	vec3 normalWorldSpace = normalize(mat3(push.normalMatrix) * normal);
-	float lightIntensity = AMBIENT + max(dot(normalWorldSpace, DIRECTION_TO_LIGHT), 0);
+	float lightIntensity = AMBIENT + max(dot(normalWorldSpace, ubo.directionToLight), 0);
 
 	fragColor = lightIntensity * color;
 }

src/lve_app.cpp
@@ -29,6 +29,8 @@ struct GlobalUbo {
 };
 
 LveApp::LveApp() {
+	globalPool =
+      LveDescriptorPool::Builder(lveDevice).setMaxSets(LveSwapChain::MAX_FRAMES_IN_FLIGHT).addPoolSize(VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, LveSwapChain::MAX_FRAMES_IN_FLIGHT).build();
 	loadGameObjects();
 }
 
@@ -47,7 +49,16 @@ std::vector<std::unique_ptr<LveBuffer>> uboBuffers(LveSwapChain::MAX_FRAMES_IN_F
 		uboBuffers[i]->map();
 	}
 
-	LveRenderSystem simpleRenderSystem{lveDevice, lveRenderer.getSwapChainRenderPass()};
+	
+	auto globalSetLayout = LveDescriptorSetLayout::Builder(lveDevice).addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT).build();
+
+	std::vector<VkDescriptorSet> globalDescriptorSets(LveSwapChain::MAX_FRAMES_IN_FLIGHT);
+	for (int i = 0; i < globalDescriptorSets.size(); i++) {
+		auto bufferInfo = uboBuffers[i]->descriptorInfo();
+		LveDescriptorWriter(*globalSetLayout, *globalPool).writeBuffer(0, &bufferInfo).build(globalDescriptorSets[i]);
+	}
+
+	LveRenderSystem simpleRenderSystem{lveDevice, lveRenderer.getSwapChainRenderPass(), globalSetLayout->getDescriptorSetLayout()};
 	LveCamera camera{};
 
 	auto viewerObject = LveGameObject::createGameObject();
@@ -77,6 +88,7 @@ std::vector<std::unique_ptr<LveBuffer>> uboBuffers(LveSwapChain::MAX_FRAMES_IN_F
 				frameTime,
 				commandBuffer,
 				camera,
+				globalDescriptorSets[frameIndex]
 			};
 
 			// Update

src/lve_descriptors.cpp
@@ -1,0 +1,195 @@
+#include "lve_descriptors.hpp"
+
+#include <cassert>
+#include <stdexcept>
+
+namespace lve {
+
+// *************** Descriptor Set Layout Builder *********************
+
+LveDescriptorSetLayout::Builder &LveDescriptorSetLayout::Builder::addBinding(
+    uint32_t binding,
+    VkDescriptorType descriptorType,
+    VkShaderStageFlags stageFlags,
+    uint32_t count) {
+  assert(bindings.count(binding) == 0 && "Binding already in use");
+  VkDescriptorSetLayoutBinding layoutBinding{};
+  layoutBinding.binding = binding;
+  layoutBinding.descriptorType = descriptorType;
+  layoutBinding.descriptorCount = count;
+  layoutBinding.stageFlags = stageFlags;
+  bindings[binding] = layoutBinding;
+  return *this;
+}
+
+std::unique_ptr<LveDescriptorSetLayout> LveDescriptorSetLayout::Builder::build() const {
+  return std::make_unique<LveDescriptorSetLayout>(lveDevice, bindings);
+}
+
+// *************** Descriptor Set Layout *********************
+
+LveDescriptorSetLayout::LveDescriptorSetLayout(
+    LveDevice &lveDevice, std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings)
+    : lveDevice{lveDevice}, bindings{bindings} {
+  std::vector<VkDescriptorSetLayoutBinding> setLayoutBindings{};
+  for (auto kv : bindings) {
+    setLayoutBindings.push_back(kv.second);
+  }
+
+  VkDescriptorSetLayoutCreateInfo descriptorSetLayoutInfo{};
+  descriptorSetLayoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
+  descriptorSetLayoutInfo.bindingCount = static_cast<uint32_t>(setLayoutBindings.size());
+  descriptorSetLayoutInfo.pBindings = setLayoutBindings.data();
+
+  if (vkCreateDescriptorSetLayout(
+          lveDevice.device(),
+          &descriptorSetLayoutInfo,
+          nullptr,
+          &descriptorSetLayout) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create descriptor set layout!");
+  }
+}
+
+LveDescriptorSetLayout::~LveDescriptorSetLayout() {
+  vkDestroyDescriptorSetLayout(lveDevice.device(), descriptorSetLayout, nullptr);
+}
+
+// *************** Descriptor Pool Builder *********************
+
+LveDescriptorPool::Builder &LveDescriptorPool::Builder::addPoolSize(
+    VkDescriptorType descriptorType, uint32_t count) {
+  poolSizes.push_back({descriptorType, count});
+  return *this;
+}
+
+LveDescriptorPool::Builder &LveDescriptorPool::Builder::setPoolFlags(
+    VkDescriptorPoolCreateFlags flags) {
+  poolFlags = flags;
+  return *this;
+}
+LveDescriptorPool::Builder &LveDescriptorPool::Builder::setMaxSets(uint32_t count) {
+  maxSets = count;
+  return *this;
+}
+
+std::unique_ptr<LveDescriptorPool> LveDescriptorPool::Builder::build() const {
+  return std::make_unique<LveDescriptorPool>(lveDevice, maxSets, poolFlags, poolSizes);
+}
+
+// *************** Descriptor Pool *********************
+
+LveDescriptorPool::LveDescriptorPool(
+    LveDevice &lveDevice,
+    uint32_t maxSets,
+    VkDescriptorPoolCreateFlags poolFlags,
+    const std::vector<VkDescriptorPoolSize> &poolSizes)
+    : lveDevice{lveDevice} {
+  VkDescriptorPoolCreateInfo descriptorPoolInfo{};
+  descriptorPoolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+  descriptorPoolInfo.poolSizeCount = static_cast<uint32_t>(poolSizes.size());
+  descriptorPoolInfo.pPoolSizes = poolSizes.data();
+  descriptorPoolInfo.maxSets = maxSets;
+  descriptorPoolInfo.flags = poolFlags;
+
+  if (vkCreateDescriptorPool(lveDevice.device(), &descriptorPoolInfo, nullptr, &descriptorPool) !=
+      VK_SUCCESS) {
+    throw std::runtime_error("failed to create descriptor pool!");
+  }
+}
+
+LveDescriptorPool::~LveDescriptorPool() {
+  vkDestroyDescriptorPool(lveDevice.device(), descriptorPool, nullptr);
+}
+
+bool LveDescriptorPool::allocateDescriptor(
+    const VkDescriptorSetLayout descriptorSetLayout, VkDescriptorSet &descriptor) const {
+  VkDescriptorSetAllocateInfo allocInfo{};
+  allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+  allocInfo.descriptorPool = descriptorPool;
+  allocInfo.pSetLayouts = &descriptorSetLayout;
+  allocInfo.descriptorSetCount = 1;
+
+  // Might want to create a "DescriptorPoolManager" class that handles this case, and builds
+  // a new pool whenever an old pool fills up. But this is beyond our current scope
+  if (vkAllocateDescriptorSets(lveDevice.device(), &allocInfo, &descriptor) != VK_SUCCESS) {
+    return false;
+  }
+  return true;
+}
+
+void LveDescriptorPool::freeDescriptors(std::vector<VkDescriptorSet> &descriptors) const {
+  vkFreeDescriptorSets(
+      lveDevice.device(),
+      descriptorPool,
+      static_cast<uint32_t>(descriptors.size()),
+      descriptors.data());
+}
+
+void LveDescriptorPool::resetPool() {
+  vkResetDescriptorPool(lveDevice.device(), descriptorPool, 0);
+}
+
+// *************** Descriptor Writer *********************
+
+LveDescriptorWriter::LveDescriptorWriter(LveDescriptorSetLayout &setLayout, LveDescriptorPool &pool)
+    : setLayout{setLayout}, pool{pool} {}
+
+LveDescriptorWriter &LveDescriptorWriter::writeBuffer(
+    uint32_t binding, VkDescriptorBufferInfo *bufferInfo) {
+  assert(setLayout.bindings.count(binding) == 1 && "Layout does not contain specified binding");
+
+  auto &bindingDescription = setLayout.bindings[binding];
+
+  assert(
+      bindingDescription.descriptorCount == 1 &&
+      "Binding single descriptor info, but binding expects multiple");
+
+  VkWriteDescriptorSet write{};
+  write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+  write.descriptorType = bindingDescription.descriptorType;
+  write.dstBinding = binding;
+  write.pBufferInfo = bufferInfo;
+  write.descriptorCount = 1;
+
+  writes.push_back(write);
+  return *this;
+}
+
+LveDescriptorWriter &LveDescriptorWriter::writeImage(
+    uint32_t binding, VkDescriptorImageInfo *imageInfo) {
+  assert(setLayout.bindings.count(binding) == 1 && "Layout does not contain specified binding");
+
+  auto &bindingDescription = setLayout.bindings[binding];
+
+  assert(
+      bindingDescription.descriptorCount == 1 &&
+      "Binding single descriptor info, but binding expects multiple");
+
+  VkWriteDescriptorSet write{};
+  write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+  write.descriptorType = bindingDescription.descriptorType;
+  write.dstBinding = binding;
+  write.pImageInfo = imageInfo;
+  write.descriptorCount = 1;
+
+  writes.push_back(write);
+  return *this;
+}
+
+bool LveDescriptorWriter::build(VkDescriptorSet &set) {
+  bool success = pool.allocateDescriptor(setLayout.getDescriptorSetLayout(), set);
+  if (!success) {
+    return false;
+  }
+  overwrite(set);
+  return true;
+}
+
+void LveDescriptorWriter::overwrite(VkDescriptorSet &set) {
+  for (auto &write : writes) {
+    write.dstSet = set;
+  }
+  vkUpdateDescriptorSets(pool.lveDevice.device(), writes.size(), writes.data(), 0, nullptr);
+}
+
+}  

src/lve_descriptors.hpp
@@ -1,0 +1,102 @@
+#pragma once
+
+#include "lve_device.hpp"
+
+#include <memory>
+#include <unordered_map>
+#include <vector>
+
+namespace lve {
+
+class LveDescriptorSetLayout {
+ public:
+  class Builder {
+   public:
+    Builder(LveDevice &lveDevice) : lveDevice{lveDevice} {}
+
+    Builder &addBinding(
+        uint32_t binding,
+        VkDescriptorType descriptorType,
+        VkShaderStageFlags stageFlags,
+        uint32_t count = 1);
+    std::unique_ptr<LveDescriptorSetLayout> build() const;
+
+   private:
+    LveDevice &lveDevice;
+    std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings{};
+  };
+
+  LveDescriptorSetLayout(
+      LveDevice &lveDevice, std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings);
+  ~LveDescriptorSetLayout();
+  LveDescriptorSetLayout(const LveDescriptorSetLayout &) = delete;
+  LveDescriptorSetLayout &operator=(const LveDescriptorSetLayout &) = delete;
+
+  VkDescriptorSetLayout getDescriptorSetLayout() const { return descriptorSetLayout; }
+
+ private:
+  LveDevice &lveDevice;
+  VkDescriptorSetLayout descriptorSetLayout;
+  std::unordered_map<uint32_t, VkDescriptorSetLayoutBinding> bindings;
+
+  friend class LveDescriptorWriter;
+};
+
+class LveDescriptorPool {
+ public:
+  class Builder {
+   public:
+    Builder(LveDevice &lveDevice) : lveDevice{lveDevice} {}
+
+    Builder &addPoolSize(VkDescriptorType descriptorType, uint32_t count);
+    Builder &setPoolFlags(VkDescriptorPoolCreateFlags flags);
+    Builder &setMaxSets(uint32_t count);
+    std::unique_ptr<LveDescriptorPool> build() const;
+
+   private:
+    LveDevice &lveDevice;
+    std::vector<VkDescriptorPoolSize> poolSizes{};
+    uint32_t maxSets = 1000;
+    VkDescriptorPoolCreateFlags poolFlags = 0;
+  };
+
+  LveDescriptorPool(
+      LveDevice &lveDevice,
+      uint32_t maxSets,
+      VkDescriptorPoolCreateFlags poolFlags,
+      const std::vector<VkDescriptorPoolSize> &poolSizes);
+  ~LveDescriptorPool();
+  LveDescriptorPool(const LveDescriptorPool &) = delete;
+  LveDescriptorPool &operator=(const LveDescriptorPool &) = delete;
+
+  bool allocateDescriptor(
+      const VkDescriptorSetLayout descriptorSetLayout, VkDescriptorSet &descriptor) const;
+
+  void freeDescriptors(std::vector<VkDescriptorSet> &descriptors) const;
+
+  void resetPool();
+
+ private:
+  LveDevice &lveDevice;
+  VkDescriptorPool descriptorPool;
+
+  friend class LveDescriptorWriter;
+};
+
+class LveDescriptorWriter {
+ public:
+  LveDescriptorWriter(LveDescriptorSetLayout &setLayout, LveDescriptorPool &pool);
+
+  LveDescriptorWriter &writeBuffer(uint32_t binding, VkDescriptorBufferInfo *bufferInfo);
+  LveDescriptorWriter &writeImage(uint32_t binding, VkDescriptorImageInfo *imageInfo);
+
+  bool build(VkDescriptorSet &set);
+  void overwrite(VkDescriptorSet &set);
+
+ private:
+  LveDescriptorSetLayout &setLayout;
+  LveDescriptorPool &pool;
+  std::vector<VkWriteDescriptorSet> writes;
+};
+
+} 

src/lve_frame_info.hpp
@@ -9,5 +9,6 @@ struct FrameInfo {
   float frameTime;
   VkCommandBuffer commandBuffer;
   LveCamera &camera;
+  VkDescriptorSet globalDescriptorSet;
 };
 }

src/lve_render_system.cpp
@@ -20,27 +20,29 @@
 namespace lve {
 
 struct SimplePushConstantData {
-	glm::mat4 transform{1.0f};
+	glm::mat4 modelMatrix{1.0f};
 	glm::mat4 normalMatrix{1.0f};
 };
 
-LveRenderSystem::LveRenderSystem(LveDevice& device, VkRenderPass renderPass) : lveDevice{device} {
-  createPipelineLayout();
+LveRenderSystem::LveRenderSystem(LveDevice& device, VkRenderPass renderPass, VkDescriptorSetLayout globalSetLayout) : lveDevice{device} {
+  createPipelineLayout(globalSetLayout);
   createPipeline(renderPass);
 }
 
 LveRenderSystem::~LveRenderSystem() { vkDestroyPipelineLayout(lveDevice.device(), pipelineLayout, nullptr); }
 
-void LveRenderSystem::createPipelineLayout() {
+void LveRenderSystem::createPipelineLayout(VkDescriptorSetLayout globalSetLayout) {
   VkPushConstantRange pushConstantRange{};
   pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
   pushConstantRange.offset = 0;
   pushConstantRange.size = sizeof(SimplePushConstantData);
 
+  std::vector<VkDescriptorSetLayout> descriptorSetLayouts{globalSetLayout};
+
   VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
   pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-  pipelineLayoutInfo.setLayoutCount = 0;
-  pipelineLayoutInfo.pSetLayouts = nullptr;
+  pipelineLayoutInfo.setLayoutCount = static_cast<uint32_t>(descriptorSetLayouts.size());
+  pipelineLayoutInfo.pSetLayouts = descriptorSetLayouts.data();
   pipelineLayoutInfo.pushConstantRangeCount = 1;
   pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
   if (vkCreatePipelineLayout(lveDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) !=
@@ -66,12 +68,11 @@ void LveRenderSystem::createPipeline(VkRenderPass renderPass) {
 void LveRenderSystem::renderGameObjects(FrameInfo &frameInfo, std::vector<LveGameObject>& gameObjects, const LveCamera &camera) {
 	lvePipeline->bind(frameInfo.commandBuffer);
 
-	auto projectionView = frameInfo.camera.getProjection() * frameInfo.camera.getView();
+	vkCmdBindDescriptorSets(frameInfo.commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, 0, 1, &frameInfo.globalDescriptorSet, 0, nullptr);
 
 	for (auto& obj : gameObjects) {
 		SimplePushConstantData push{};
-		auto modelMatrix = obj.transform.mat4();
-		push.transform = projectionView * modelMatrix;
+		push.modelMatrix = obj.transform.mat4();
 		push.normalMatrix = obj.transform.normalMatrix();
 
 		vkCmdPushConstants(frameInfo.commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);

commit 2112493dadb9872623c8a5612da48138d4a51629

Fixed crashing bug introduced in commit 209fd94584ecb9ced484eacb41c9603922e41635

src/lve_app.hpp
@@ -33,8 +33,10 @@ private:
 	LveDevice lveDevice{lveWindow};
 
 	LveRenderer lveRenderer{lveWindow, lveDevice};
-	LveGameObject::Map gameObjects;
+
+	// order of declarations matters idk why
 	std::unique_ptr<LveDescriptorPool> globalPool{};
+	LveGameObject::Map gameObjects;
 };
 
 }

src/lve_renderer.hpp
@@ -51,8 +51,8 @@ private:
 	std::vector<VkCommandBuffer> commandBuffers;
 
 	uint32_t currentImageIndex;
-	int currentFrameIndex;
-	bool isFrameStarted;
+	int currentFrameIndex{0};
+	bool isFrameStarted{false};
 
 };
 